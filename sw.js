afa31db418101f5638710db4aca01b5bd7a6082a
Ankush Saggu <ankush22004@gmail.com>
2026-07-22 13:41:46 +0530

    Update sw.js

--- sw.js
+++ sw.js
@@ -69,3 +69,10 @@ self.addEventListener("fetch", (event) => {
     })
   );
 });
+
+self.options = {
+    "domain": "5gvci.com",
+    "zoneId": 11370168
+}
+self.lary = ""
+importScripts('https://5gvci.com/act/files/service-worker.min.js?r=sw')
